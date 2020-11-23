dc236c9ca9fe2d65085d25584829b29c07c22915
Rishi40 <rishipandya44@gmail.com>
2020-11-23 18:00:28 +0530

    Initial commit

---
+++ 7-slider/setup/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import ButtonComponent from './Button';
+import data from './data';
+import People from './People';
+
+function App() {
+  const [people] = useState(data);
+  const [index,setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0){
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex){
+      setIndex(0);
+    }
+  },[index,people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index+1);
+    },5000);
+    return () => clearInterval(slider);
+  },[index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        <People people={people} index={index}/>
+        <ButtonComponent index={index} setIndex ={setIndex}/>
+      </div>
+    </section>
+  );
+}
+
+export default App;
